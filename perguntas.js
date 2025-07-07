b90732f4ac154af18d6a778af05e2a7a34702042
serginhojuninho <167528719+serginhojuninho@users.noreply.github.com>
2025-07-07 11:02:13 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'minecraft',
+    'O que é minecraft?',
+    'O minecraft é um jogo'
+)
+
+criaCartao(
+    'vegetais',
+    'oque é uma moranga?',
+    'é como os sulistas chamam as abóboras'
+)
+
+criaCartao(
+    'conhecimentos gerais',
+    'quantas core tem o arco iris?',
+    'tem 7'
+)
+
+criaCartao(
+    'ciências',
+    'qual a distancia da terra a lua?',
+    'é de 384.400 km'
+)
